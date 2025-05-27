55826897520a86d63098d342b763a2d4384aa7f4
dzackiero <dzakynashshar@gmail.com>
2025-05-27 22:37:17 +0700

    Add Artisan command for fresh application setup with seeding

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command("application:fresh", function () {
+    $this->call("migrate:fresh", ["--seed" => true]);
+    $this->call("shield:super-admin");
+    $this->call("shield:generate", ["--all" => true]);
+})->purpose("Drop all tables and re-run all migrations with seeding");
