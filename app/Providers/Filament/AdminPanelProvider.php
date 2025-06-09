cedb79dbe3e6ca60fd72e30aad3f6ba66d1e4fbf
rahillahanami <rahillahanami2@gmail.com>
2025-06-09 13:17:21 +0700

    logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -39,6 +39,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandName('Maritim Muda Nusantara')
+            ->favicon(asset('images/logo-maritim.png')) 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
